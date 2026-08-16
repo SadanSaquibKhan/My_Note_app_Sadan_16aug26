a7cbe31176943ee82c8775d1f1781f447bce226d
SadanSaquibKhan <45497466+SadanSaquibKhan@users.noreply.github.com>
2026-08-16 19:35:41 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+/* Margin service worker.
+   Cache-first for the shell: once installed the app never needs the network.
+   Bump CACHE when you upload a new index.html or the old one will be served. */
+const CACHE = "margin-2026-08-15-v6";
+const SHELL = ["./", "./index.html", "./manifest.webmanifest",
+               "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
